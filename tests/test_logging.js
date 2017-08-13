5920ff0a72226d822e2a8dec18033cac3b973279
Vardan Pogosian <vardan.pogosyan@gmail.com>
2017-08-13 16:17:28 +0300

    feat(tests): add tests folder; add test_logging.js

---
+++ tests/test_logging.js
@@ -0,0 +1,13 @@
+var logMsg = "Hello, World!"
+var var1, var2,
+    var3, simpleVariable4;
+
+console.log(logMsg);
+
+var2 = "One more log message";
+
+console.log(var2);
+
+simpleVariable4 = "Info message from Node";
+
+console.info(simpleVariable4);
