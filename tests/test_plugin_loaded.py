e2cb0390ea13e1c51c5bc6878921ebb7959549db
Vardan Pogosian <vardan.pogosyan@gmail.com>
2017-08-21 14:56:51 +0300

    fix(tests): add missing files

---
+++ tests/test_plugin_loaded.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+import sublime
+from unittesting import DeferrableTestCase
+
+version = sublime.version()
+
+
+class TestPluginLoad(DeferrableTestCase):
+    def setUp(self):
+        test_js_logging_file = os.path.join(os.path.dirname(__file__), 'data', 'test_logging.js')
+        self.view = sublime.active_window().open_file(test_js_logging_file)
+        # make sure we have a window to work with
+        s = sublime.load_settings("Preferences.sublime-settings")
+        s.set("close_windows_when_empty", False)
+        
+
+    def tearDown(self):
+        pass
+
+    def testPluginIsLoaded(self):
+        self.view.window().focus_view(self.view)
+        self.view.run_command('node_run')
+        out_panel = sublime.active_window().find_output_panel('nodejs')
+        self.assertIsNotNone(out_panel)
