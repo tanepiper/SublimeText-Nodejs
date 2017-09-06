ccd010803557dbb15a55712531d46dfe066f4b5b
Vardan Pogosian <vardan.pogosyan@gmail.com>
2017-09-06 08:35:12 +0300

    feat(nodejs_debug): add info() function

--- lib/nodejs_debug.py
+++ lib/nodejs_debug.py
@@ -1,10 +1,23 @@
 import os
+import datetime
 import logging
 
 from .nodejs_constants import PLUGIN_DEBUG_FILE
 
+PLUGIN_STR  = 'Nodejs Plugin'
+DEBUG_LEVEL = 'DEBUG'
+INFO_LEVEL  = 'INFO'
+
+
+def cur_time():
+    return datetime.datetime.now().isoformat()
+
 # global debug function
 def debug(key, value):
-    is_debug = True if os.path.exists(PLUGIN_DEBUG_FILE) else False   
+    is_debug = True if os.path.exists(PLUGIN_DEBUG_FILE) else False
     if is_debug:
-        logging.debug("Nodejs Plugin - {0} {1}".format(key, value))
+        print("{0} - {1} - {2} => {3} - {4}".format(PLUGIN_STR, 
+                                            DEBUG_LEVEL, cur_time(), key, value))
+
+def info(msg):
+    print("{0} - {1} - {2} => {3}".format(PLUGIN_STR, INFO_LEVEL, cur_time(), msg))
