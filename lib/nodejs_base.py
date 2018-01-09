eff22cfb4dcba5decd5e6c2e4a91ce2640f0faba
Vardan Pogosian <vardan.pogosyan@gmail.com>
2018-01-09 11:22:33 +0300

    fix(nodejs_base): use self.__class__.__name__ for derived classes commands

--- lib/nodejs_base.py
+++ lib/nodejs_base.py
@@ -49,10 +49,12 @@ class NodeCommand(sublime_plugin.TextCommand):
             shell = True
             # doing exception for debugger commands to be able 
             # get PID of node.exe on Windows not cmd.exe
-            if __class__.__name__.lower().find("drun") != -1:
+            debug("NodeCommand: run_command: class name", self.__class__.__name__)
+            if self.__class__.__name__.lower().find("drun") != -1:
                 shell = False
         
         kwargs['shell'] = shell
+        debug("NodeCommand: run_command: kwargs", kwargs)
 
         thread = CommandThread(command, callback, **kwargs)
         thread.start()
